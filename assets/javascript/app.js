281a474064a1c07f7972d4f35dfffbe403c3993b
Matt White <mattwhite614@gmail.com>
2016-06-06 20:32:47 -0500

    MVP done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,41 @@
+//connect to my firebase
 var myFirebase = new Firebase('https://fb-train-schedule.firebaseio.com/');
 
-
+// grab data from form
 $('#submit').on('click', function(){
   var train = $('#train_name').val().trim();
   var destination = $('#destination').val().trim();
   var frequency = $('#frequency').val().trim();
   var firstTrain = $('#firstTrain').val().trim();
 
+  //add data from form and add it to firebase DB
   myFirebase.push({
               trainName: train,
               destination: destination,
               frequency: frequency,
               firstTrain: Firebase.ServerValue.TIMESTAMP
-          });
+          })
+});
 
-})
-
+//display data
 myFirebase.on("child_added", function(childSnapshot) {
-  //do some math here
+  // find when the next train is and minutes until next train
+  var tfrequency = childSnapshot.val().frequency;
+  var convertedDate = moment(new Date(childSnapshot.val().firstTrain));
+  var trainTime = moment(convertedDate).format("HH:mm");
+  var currentTime = moment();
+  var firstTimeConverted = moment(trainTime,"hh:mm").subtract(1, "years");
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var tRemainder = diffTime % tfrequency;
+  //solved
+  var tMinutesTillTrain = tfrequency - tRemainder;
+  //solved
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("HH:mm")
 
+  //append DOM
   $('#schedule').append("<tr><td>" + childSnapshot.val().trainName + "</td><td>" +
   childSnapshot.val().destination + "</td><td>" + childSnapshot.val().frequency +
-  "</td><td>" + childSnapshot.val().firstTrain + "</td></tr>")
-}, function(errorObject) {
+  "</td><td>" + trainTime + "</td><td>" + tMinutesTillTrain + "</td></tr>")
+  }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
-})
+  })
